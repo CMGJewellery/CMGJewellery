36a51236da04c56b421173bda98ab8814eedb8c9
Yen Minh <minhhy2897@gmail.com>
2018-05-02 11:01:03 +0700

    generate admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'user/edit'
+
+
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations',
 
   }
+  scope :admin do
+    resources :users
+  end
+
   resources :images
   resources :products
   resources :style_of_products
